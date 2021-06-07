17fdc782e2c3a3182d0ce88a6e084be06c299fee
andreepratama27 <andre.pratama@accelbyte.net>
2021-06-07 17:32:21 +0700

    feat: Add Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Box,
+  ChakraProvider,
+  Container,
+  Heading,
+  Stack,
+} from "@chakra-ui/react";
+
+enum Month {
+  January,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <Container>
+          <Box
+            bg="blue.400"
+            h="36"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            p="10px 20px"
+          >
+            <Heading color="white">Hooliday</Heading>
+          </Box>
+
+          <Box p="14px 0">
+            <Heading size="md" p="8px 0 14px 0">
+              Month in a year
+            </Heading>
+
+            <Stack spacing="6">
+              {Object.keys(Month).map((key) => (
+                <Box
+                  key={key}
+                  borderRadius="md"
+                  backgroundColor="blue.100"
+                  p="14px"
+                >
+                  <Heading color="blue.900" size="sm">
+                    {Month[1]}
+                  </Heading>
+                </Box>
+              ))}
+            </Stack>
+          </Box>
+        </Container>
+      </div>
+    </ChakraProvider>
   );
 }
 
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+interface Home {}
+
+const Home: React.FC<Home> = () => {
+  return (
+    <div>
+      <h1>Welcome Home</h1>
+    </div>
+  );
+};
+
+export default Home;
